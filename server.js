56133e25264bada8ccc2a442e4b5a5d5d814243a
Ashleigh Maund <ashleigh.maund@bcsg.com>
2016-12-22 17:25:33 +0000

    amended server.js file, no longer trying to compress here

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 var http = require('http');
 
 var express = require('express');
-var h5bp = require('h5bp');
 
 var server = express();
-var compression = require('compression');
-
-server.use(h5bp({ root: __dirname + '/public' }));
+server.use(express.static(__dirname + '/'));
 server.use('/page1', express.static(__dirname + '/page1.html'));
 server.use('/page2', express.static(__dirname + '/page2.html'));
 server.use('/page3', express.static(__dirname + '/page3.html'));
 server.use('/page4', express.static(__dirname + '/page4.html'));
-
-// server.listen(3000);
 
 var port = 5000;
 server.listen(process.env.PORT || port);
